5dd9f754d93165b22f6a5df4c5661f4d1d35981e
Igor Kolpakov <kolpakovigorjob@yandex.ru>
2022-10-08 12:50:24 +0300

    fix git dependendensys

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,26 +1,31 @@
 import Post from "./Post";
-import {FlatList, View, StyleSheet} from "react-native";
+import { FlatList, View, StyleSheet } from "react-native";
 import Header from "./Header";
 
-const Posts = ({loggedIn, setLoggedIn, allUsersFirstInfo}) => {
-    const renderPosts = ({item}) => {
-        return <Post postData={item}/>
-    };
+const Posts = ({ loggedIn, setLoggedIn, allUsersFirstInfo }) => {
+  const renderPosts = ({ item }) => {
+    return <Post postData={item} />;
+  };
 
-    return (
-        <View style={style.postListWrapper}>
-            <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
-            <FlatList data={allUsersFirstInfo} renderItem={renderPosts} style={style.postList}/>
-        </View>
-    )
-}
+  return (
+    <View style={style.postListWrapper}>
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      <FlatList
+        data={allUsersFirstInfo}
+        renderItem={renderPosts}
+        style={style.postList}
+      />
+    </View>
+  );
+};
 const style = StyleSheet.create({
-    postList:{
-        height:'100%'
-    },
-    postListWrapper:{
-        height:'100%'
-    }
-})
+  postList: {
+    height: "100%",
+    flex: 1,
+  },
+  postListWrapper: {
+    height: "100%",
+  },
+});
 
 export default Posts;
